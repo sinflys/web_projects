1d2edb8951e1decc95aedfc6cdc26b8d7e493157
ilho <sinflys@naver.com>
2021-11-17 15:17:09 +0900

    11월17일오후

---
+++ MainMethood/src/application/test/App.java
@@ -0,0 +1,13 @@
+package application.test;
+
+public class App {
+
+	public static void main(String[] args) {
+		// 메인 메소드
+		// 첫 자바 프로그램 실행
+		System.out.println("안녕하세요");
+		System.out.println("새 자바 프로그램을");
+		System.out.println("실행 합니다.");
+	}
+
+}
